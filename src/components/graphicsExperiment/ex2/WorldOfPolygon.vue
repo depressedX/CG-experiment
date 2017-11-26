b845fc4adf87515bd282d155337989affc9903db
depressedX <yuri8023@qq.com>
2017-11-26 23:27:28 +0800

    wordOfPolygon.js

---
+++ src/components/graphicsExperiment/ex2/WorldOfPolygon.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <canvas ref="targetCanvas"/>
+  </div>
+</template>
+<script>
+  export default {
+    components: {},
+    created() {
+
+    },
+    data() {
+      return {}
+    },
+    props: {},
+    computed: {},
+    watch: {},
+    methods: {}
+  }
+</script>
+<style>
+
+</style>
---
+++ src/components/graphicsExperiment/ex2/wordOfPolygon.js
@@ -0,0 +1,24 @@
+import * as Three from 'three'
+
+function WordOfPolygon(options) {
+  /*私有变量*/
+  let targetCanvas = null,
+    camera = null,
+    renderer = null,
+    scene = null
+
+
+
+
+  function wordOfPolygon(options) {
+
+    scene = new Three.Scene
+
+  }
+  wordOfPolygon.prototype = {
+    constructor:wordOfPolygon,
+
+  }
+
+  return new wordOfPolygon(options)
+}
