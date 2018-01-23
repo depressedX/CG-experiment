1c7e9d385e67c32c17cc8940dfc4dfbbcac74947
lph <yuri8023@qq.com>
2018-01-23 15:19:02 +0800

    ..........

--- src/components/graphicsExperiment/ex3/consts.js
+++ src/components/graphicsExperiment/ex3/consts.js
@@ -1,12 +1,12 @@
-export const TRACK_SIZE = 1600
-export const TRACK_WIDTH = 200
+export const TRACK_SIZE = 600
+export const TRACK_WIDTH = 100
 
 export const FOV = 60
 export const ASPECT = 16 / 9
 
-export const SCENE_WIDTH = 1600
-export const SCENE_HEIGHT = 1600
-export const SCENE_DEPTH = 1600
+export const SCENE_WIDTH = 600
+export const SCENE_HEIGHT = 600
+export const SCENE_DEPTH = 600
 
 
 export default {
